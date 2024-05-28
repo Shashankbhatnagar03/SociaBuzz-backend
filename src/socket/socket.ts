4217294601d2bd723bbff34616eb6cb7d946a966
Shashankbhatnagar03 <Shashank.bhatnagar3@gmail.com>
2024-05-28 17:39:00 +0530

    Deploy

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -8,8 +8,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: "https://socia-buzzz.netlify.app",
+    methods: "*",
   },
 });
 
