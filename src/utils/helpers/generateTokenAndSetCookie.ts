bf2c7e35910605b32aefeb495d306f9529d54286
Shashankbhatnagar03 <Shashank.bhatnagar3@gmail.com>
2024-05-31 13:03:29 +0530

    cookie settings updated

--- src/utils/helpers/generateTokenAndSetCookie.ts
+++ src/utils/helpers/generateTokenAndSetCookie.ts
@@ -16,6 +16,7 @@ const generateTokenAndSetCookie = (userId: Types.ObjectId, res: Response) => {
     maxAge: 15 * 24 * 60 * 60 * 1000, // 30 days
     sameSite: "none",
     secure: true,
+    path: "/",
   });
 
   return token;
