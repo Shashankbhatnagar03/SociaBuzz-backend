8d6d383f203050be65b544fe781a859264a2ed6d
Shashankbhatnagar03 <Shashank.bhatnagar3@gmail.com>
2024-11-18 17:30:21 +0530

    Added function to make the server working

--- src/app.ts
+++ src/app.ts
@@ -24,3 +24,13 @@ app.use("/api/v1/messages", messageRoutes);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log("server started at local host " + PORT));
+setInterval(async () => {
+  console.log(" calling ping ");
+  try {
+    const response = await fetch(process.env.PING_URL || "");
+    const data = await response.text();
+    console.log("response  ", data);
+  } catch (e) {
+    console.log("error in ping ", e);
+  }
+}, 840000);
